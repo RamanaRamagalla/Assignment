2f3de768bbc94d69be78f20969a0ecaff4531959
RamanaRamagalla <ramanaramagalla007@gmail.com>
2024-11-21 19:59:59 +0530

    Update Login.jsx

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
         } else {
             setError('');
             try {
-                const response = await fetch('http://localhost:5000/login', {
+                const response = await fetch('https://assignment-1-6idx.onrender.com/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
