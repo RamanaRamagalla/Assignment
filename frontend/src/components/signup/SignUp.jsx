ae65e8739673054b26b68f234e11fae55475c045
RamanaRamagalla <ramanaramagalla007@gmail.com>
2024-11-21 20:00:30 +0530

    Update SignUp.jsx

--- frontend/src/components/signup/SignUp.jsx
+++ frontend/src/components/signup/SignUp.jsx
@@ -20,7 +20,7 @@ const SignUp = () => {
         } else {
             setError('');
             try {
-                const response = await fetch('http://localhost:5000/signup', {
+                const response = await fetch('https://assignment-1-6idx.onrender.com/signup', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
